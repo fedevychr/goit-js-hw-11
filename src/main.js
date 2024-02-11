a1b281fcc62b2a69785fd9aa0374139b3930581e
Roman Fedevych <fedevychr@gmail.com>
2024-02-12 01:29:01 +0200

    add1

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_URL = 'https://pixabay.com/api/';
+
